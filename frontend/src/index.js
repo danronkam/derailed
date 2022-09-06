8a51b7ec79773f62755e953ea52a84075920eb26
“NAME” <EMAIL@EMAIL.COM>
2022-09-06 11:58:40 -0700

    frontend phase 24

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,13 +8,14 @@ import App from './App';
 import configureStore from './store';
 import csrfFetch, { restoreCSRF } from './store/csrf';
 // import csrfFetch from './store/csrf';
-// import * as sessionActions from './store/session';
+import * as sessionActions from './store/session';
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== "production") {
   window.store = store;
   window.csrfFetch = csrfFetch;
+  window.sessionActions = sessionActions;
 }
 
 function Root() {
