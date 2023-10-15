227bb799448fb4ac7506e5be43e3fd16ca2f86b0
danronkam <danronkam@gmail.com>
2023-10-14 19:22:15 -0700

    im making moves

--- frontend/src/components/Slider/index.js
+++ frontend/src/components/Slider/index.js
@@ -5,75 +5,23 @@ import { useState } from "react";
 import { useEffect } from "react";
 
 function Slider()  {
-    const content =[
-        {title : 'Shop Accessories', blurb: 'PUT A RING ON IT', path: '/shop/menswear/:Accessories', photoUrl: 'https://derailed-seed.s3.us-west-1.amazonaws.com/rings.png'},
-        {title: 'Shop Outerwear', blurb: 'TOO ICEY', path: '/shop/menswear/:Outerwear', photoUrl: 'https://derailed-seed.s3.us-west-1.amazonaws.com/puffer.png'},
-        {title: 'Get Sneakers Under Retail', blurb: 'SNEAKER STEALS', path: '/shop/menswear/:Sneakers', photoUrl: 'https://derailed-seed.s3.us-west-1.amazonaws.com/travi.jpg'}   
-    ]
+    const content = {
+        title: 'THE PLATFORM FOR PERSONAL STYLE',
+        sub: "Buy, sell, discover authenticated pieces from the world's top brands.",
+        butt1: {text: "SHOP MENSWEAR", src:"/mens"},
+        butt2: {text: "SHOP WOMANS", src: "/womans"}
+    }
     const [isLoading, setIsLoading] = useState(true);
-
+    
 
 
     let sliderIdx = 2
     useEffect(() => {
         const timer = setTimeout(function () {
             setIsLoading(false);
-            nextSlide()
           }, 1000);
           return () => clearTimeout(timer);
             }, []);
-
-    //  titles = currentSlide[:title]
-
-
-    function nextSlide() {
-        var slides = document.getElementsByClassName("Containers");
-        // var circles = document.getElementsByClassName("dots");
-
-        let i;
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-        }
-
-        if(!slides){
-            
-        }else if(sliderIdx !== content.length -1 ) {
-            sliderIdx += 1
-            if(!slides[sliderIdx]){
-                return
-            } else {
-                slides[sliderIdx].style.display = "block"
-            }
-        } else if(sliderIdx === 0) {
-            sliderIdx += 1
-            if(!slides[sliderIdx]){
-                return
-            } else {
-                slides[sliderIdx].style.display = "block"
-            }
-
-
-        }else {
-            sliderIdx = 0
-            if(!slides[sliderIdx]){
-                return
-            } else {
-                slides[sliderIdx].style.display = "block"
-            }
-
-        }
-        setTimeout(nextSlide, 8000)
-
-    }
-
-
-  
-    let currentSlide = content[sliderIdx]
-    let title = currentSlide.title
-    let blurb = currentSlide.blurb
-    let path = currentSlide.path
-    let photoUrl = currentSlide.photoUrl
-
 
    return(
          <>
@@ -89,51 +37,19 @@ function Slider()  {
                 <div></div>
             )}
          
-            <div className="Containers slider-container">
-                <Link to={`/shop/menswear/:Formal`}>
-                <div className='slides'>
-                    <div className="slider-left">
-                        <h3>Formal Shoes for Any Ocassion</h3>
-                        <h1>WDNG SZN</h1>
-                        <button id='shop-button'>SHOP NOW</button>
-                    </div>
-                    <div className="slider-right">
-                        <div className="MessageInfo"> test 1 </div>
-                        <img src="https://derailed-seed.s3.us-west-1.amazonaws.com/2018-02_GQ_Essential-Dress-Shoes_3x2.webp" className="slider-photo" />
-                    </div>
-                </div>
-                </Link>
-            </div>
-            <div className="Containers slider-container">
-                <Link to={`/shop/menswear/:Sneakers`}>
-                    <div className='slides'>
-                        <div className="slider-left">
-                            <h3>Unique Sneakers</h3>
-                            <h1>OUT THERE DESIGNS</h1>
-                            <button id='shop-button'>SHOP NOW</button>
+                <div className="splash-container"> 
+                    <div className="text-wrapper">
+                        <h1>{`${content.title}`}</h1>
+                        <h2>{`${content.sub}`}</h2>
+                        <div className="butt-container">
+                            <Link exact to={`${content.butt1.src}`}>{content.butt1.text}</Link>
+                            <Link exact to={`${content.butt2.src}`}>{content.butt2.text}</Link>
                         </div>
-                        <div className="slider-right">
-                            <div className="MessageInfo"> test 1 </div>
-                            <img src="https://derailed-seed.s3.us-west-1.amazonaws.com/GQ-sneakers.webp" className="slider-photo"/>
-                        </div>
+                        
                     </div>
-                </Link>
-            </div>
-            <div className="Containers slider-container">
-                <Link to='/shop'>
-                <div className='slides'>
-                    <div className="slider-left">
-                        <h3>Prep Reimagined</h3>
-                        <h1>CHUNKY LOAFERS</h1>
-                        <button id='shop-button'>SHOP NOW</button>
-                    </div>
-                    <div className="slider-right">
-                        <div className="MessageInfo"> test 1 </div>
-                        <img src="https://derailed-seed.s3.us-west-1.amazonaws.com/GQ-Loafers-Weejuns-050120.webp"  className="slider-photo" />
-                    </div>
+
+                    <img src="https://craftsnippets.com/articles_images/placeholder/placeholder.jpg" />
                 </div>
-                </Link>
-            </div>
     
 
 
