2f20760c7f7188d5be4b1f1e2e99ded94e83dd68
danronkam <danronkam@gmail.com>
2024-03-19 11:36:27 -0700

    collections grid 2x2 added, needs styling

--- frontend/src/components/FeaturedCollections/index.js
+++ frontend/src/components/FeaturedCollections/index.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom/cjs/react-router-dom"
+import './FeaturedCollections.css'
 
 const FeaturedCollections = () => {
     const Features = [
@@ -29,22 +31,83 @@ const FeaturedCollections = () => {
 
     return(
         <>
+        <h1 className="homepage-titles">
+            Featured Collections & Stories
+        </h1>
+        
         <div className="collections-container">
-            <h1 className="homepage-titles">
-                Featured Collections & Stories
-            </h1>
-            <a className="featured-item">
-                <img src="https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg">
 
-                </img>
-                <div className="featured-item-desc">
-                    <h2 className="featured-item-subhead">
-                        trend report
-                    </h2>
-                    <
-                </div>
+            <div className="featured-item">
+                <a className="featured-link" href="http://www.google.com" target="_blank">
+                    <img src="https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg">
 
-            </a>
+                    </img>
+                    <div className="featured-item-desc">
+                        <h2 className="featured-item-subhead">
+                            trend report
+                        </h2>
+        
+                        <Link className="" exact to="/mens">
+                            READ MORE
+                        </Link>
+                    </div>
+
+                </a>
+            </div>
+
+            <div className="featured-item">
+                <a className="featured-link" href="http://www.google.com" target="_blank">
+                    <img src="https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg">
+
+                    </img>
+                    <div className="featured-item-desc">
+                        <h2 className="featured-item-subhead">
+                            trend report
+                        </h2>
+        
+                        <Link className="" exact to="/mens">
+                            READ MORE
+                        </Link>
+                    </div>
+
+                </a>
+            </div>
+            <div className="featured-item">
+                <a className="featured-link" href="http://www.google.com" target="_blank">
+                    <img src="https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg">
+
+                    </img>
+                    <div className="featured-item-desc">
+                        <h2 className="featured-item-subhead">
+                            trend report
+                        </h2>
+        
+                        <Link className="" exact to="/mens">
+                            READ MORE
+                        </Link>
+                    </div>
+
+                </a>
+            </div>
+            <div className="featured-item">
+                <a className="featured-link" href="http://www.google.com" target="_blank">
+                    <img src="https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg">
+
+                    </img>
+                    <div className="featured-item-desc">
+                        <h2 className="featured-item-subhead">
+                            trend report
+                        </h2>
+        
+                        <Link className="" exact to="/mens">
+                            READ MORE
+                        </Link>
+                    </div>
+
+                </a>
+            </div>
+
+
 
         </div>
         
